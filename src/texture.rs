df2ec2c14338f72554055a9f69d0fe2f576c2b1f
nathanfaucett <nathanfaucett@gmail.com>
2016-11-15 13:12:50 -0600

    simple is pot function

--- src/texture.rs
+++ src/texture.rs
@@ -179,15 +179,16 @@ impl Texture {
 }
 
 fn is_pot(x: usize) -> bool {
-    !(
-        x != 1 && x != 2 && x != 4 && x != 8 && x != 16 && x != 32 &&
-        x != 64 && x != 128 && x != 256 && x != 512 && x != 1024 &&
-        x != 2048 && x != 4096 && x != 8192 && x != 16384 &&
-        x != 32768 && x != 65536 && x != 131072 && x != 262144 &&
-        x != 524288 && x != 1048576 && x != 2097152 &&
-        x != 4194304 && x != 8388608 && x != 16777216 &&
-        x != 33554432 && x != 67108864 && x != 134217728 &&
-        x != 268435456 && x != 536870912 && x != 1073741824 &&
-        x != 2147483648
-    )
+    (x & (-(x as isize) as usize)) == x
+}
+
+#[test]
+fn test_is_pot() {
+    assert_eq!(is_pot(2), true);
+    assert_eq!(is_pot(4), true);
+    assert_eq!(is_pot(8), true);
+    assert_eq!(is_pot(64), true);
+    assert_eq!(is_pot(1024), true);
+    assert_eq!(is_pot(11), false);
+    assert_eq!(is_pot(456), false);
 }
